5206782506039f4df4c387b53340c49d6f95dda0
retrazil <retrazil@gmail.com>
2017-05-08 11:03:24 +0530

    refactor: start with typeof(obj) === object

--- deepEqual.js
+++ deepEqual.js
@@ -1,3 +1,4 @@
+// Your code here.
 function length(obj){
 	var count = 0; 
   	for(var key in obj){
@@ -7,33 +8,31 @@ function length(obj){
 }
 
 function deepEqual(obj1, obj2){
-  	if(obj1 === null || obj2 === null){
-    	return false; 
-    } else if(typeof(obj1) !== typeof(obj2)){
-      	return false; 
-    } else if(typeof(obj1) === "object"){  
-      	if(length(obj1) !== length(obj2)){
-        	return false; 
-        } else {
-        	for(var key in obj1){
-              	console.log("key in obj1: " + key  + ":" + obj1[key]);
-              	console.log("key in obj2: "  + key + ":" + obj2[key]);
-            	if(!obj2.hasOwnProperty(key)){
-                	return false; 
-                }
-            }
-        }
-    	return true;
-    } else {
+ if(typeof(obj1) === "object" || typeof(obj2) === "object"){
+   	if(obj1 === null || obj2 === null) 
+      return false; 
+    
+   if(length(obj1) !== length(obj2)){
+        return false; 
+       } 
+   else {
+        for(var key in obj1){
+            if(!obj2.hasOwnProperty(key)){
+                return false; 
+               } 
+           }
+       }
+    return true;
+ } else {
     	return obj1 === obj2; 
-    }  	
+   }  	
 }
 
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, {here: "comes", the: "sun"}));
 
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
 // → true
-//console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
-//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
