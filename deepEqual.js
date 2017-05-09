05f4eb01910ff6ebea2eea5365587c56e4f5b30b
retrazil <retrazil@gmail.com>
2017-05-09 11:06:33 +0530

    refactor and use || for multiple if statements

--- deepEqual.js
+++ deepEqual.js
@@ -2,43 +2,31 @@ function length(obj){
 	var count = 0; 
   	for(var key in obj){
     	count++;
+function length(obj){
+	var count = 0; 
+  	for(var key in obj){
+    	count++;
     }
   	return count; 
 }
 
 function deepEqual(obj1, obj2){
  if(typeof(obj1) === "object" || typeof(obj2) === "object"){
-   	if(obj1 === null || obj2 === null) // typeof(null) is an object
-      return false; 
-    
-   if(length(obj1) !== length(obj2)){
+   	if(obj1 === null || obj2 === null || // type of null is "object"
+       length(obj1) !== length(obj2)){
         return false; 
        } 
-   else {
-        for(var key in obj1){
-          // check if keys in obj1 are present in obj2 (values maybe different)
-            if(!obj2.hasOwnProperty(key)){ 
-                return false; 
-               } 
-          	// return false as soon as first inequality between 
-          	// object values is found in recursive call
-          	if(!deepEqual(obj1[key], obj2[key])){
-            	return false; 
+   
+    for(var key in obj1){
+       // check if keys in obj1 are present in obj2 (values maybe different)
+       if(!obj2.hasOwnProperty(key) ||
+         !deepEqual(obj1[key], obj2[key])){
+           return false; 
             }
-           }
-       }
+           }       
     return true;
  } else {
    	// return this if typeof var is not an object
     	return obj1 === obj2; 
    }  	
 }
-
-
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
