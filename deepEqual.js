bf3a273ea340781756af7a2dcb133c68214f95a6
retrazil <retrazil@gmail.com>
2017-05-09 10:55:14 +0530

    add comments

--- deepEqual.js
+++ deepEqual.js
@@ -8,7 +8,7 @@ function length(obj){
 
 function deepEqual(obj1, obj2){
  if(typeof(obj1) === "object" || typeof(obj2) === "object"){
-   	if(obj1 === null || obj2 === null) 
+   	if(obj1 === null || obj2 === null) // typeof(null) is an object
       return false; 
     
    if(length(obj1) !== length(obj2)){
@@ -16,9 +16,12 @@ function deepEqual(obj1, obj2){
        } 
    else {
         for(var key in obj1){
-            if(!obj2.hasOwnProperty(key)){
+          // check if keys in obj1 are present in obj2 (values maybe different)
+            if(!obj2.hasOwnProperty(key)){ 
                 return false; 
                } 
+          	// return false as soon as first inequality between 
+          	// object values is found in recursive call
           	if(!deepEqual(obj1[key], obj2[key])){
             	return false; 
             }
@@ -26,6 +29,7 @@ function deepEqual(obj1, obj2){
        }
     return true;
  } else {
+   	// return this if typeof var is not an object
     	return obj1 === obj2; 
    }  	
 }
